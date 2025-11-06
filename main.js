661784dd02b846937f62dbec13e5aa24cf33274b
Noel-Backhaus <114349123+Noel-Backhaus@users.noreply.github.com>
2025-11-06 12:11:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,52 +2,45 @@
 
 const deutsch = ["Freizeitangebote mit Ehrenamtlichen",
     "Zeit", "Montag", "Dienstag","Mittwoch", "Donnerstag", "Freitag", 
-    "11:00", "Kreativangebot", "14:00",
-    "Brettspiele", "Deutschkurs", "14:30",
-    "Sport Workout für Männer", "Sport Workout für Männer", "15:30",
-    "Sport Workout für Frauen", "Sport Workout für Frauen"];
+    "14:00", "Deutschkurs", "14:30", "Kreativangebot", 
+    "Brettspiele", "Sport Workout für Männer", "15:30",
+    "Sport Workout für Frauen"];
 
-const englisch = ["Leisure Activities with Volunteers", 
+const englisch = ["Leisure activities with volunteers",
     "Time", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
-    "11:00", "Crafting", "14:00",
-    "Board Games", "German course", "14:30",
-    "Sport workout for men", "Sport workout for men", "15:30",
-    "Sport workout for women", "Sport workout for women"];
+    "14:00", "German course", "14:30", "Crafting", 
+    "Board games", "Sports workout for men", "15:30",
+    "Sports workout for women"];
 
-const franzoesisch = ["Activités de loisirs avec des bénévoles", 
+const franzoesisch = ["Activités de loisirs avec des bénévoles",
     "Heure", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", 
-    "11h00", "Bricoler", "14h00",
-    "Jeux de société", "Cours d'allemand ", "14h30",
-    "Séance d'entraînement sportif pour hommes", "Séance d'entraînement sportif pour hommes", "15h30",
-    "Séance d'entraînement sportif pour femmes", "Séance d'entraînement sportif pour femmes"];
+    "14h00", "Cours d'allemand", "14h30", "Bricoler", 
+    "Jeux de société", "Entraînement sportif pour hommes", "15h30",
+    "Entraînement sportif pour femmes"];
 
-const tuerkisch = ["Gönüllülerle birlikte boş zaman etkinlikleri", 
-    "Saat", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", 
-    "11:00", "Yaratıcı etkinlik", "14:00",
-    "Masa oyunları", "Almanca kursu", "14:30",
-    "Erkekler için spor egzersizi", "Erkekler için spor egzersizi", "15:30",
-    "Kadınlar için spor egzersizi", "Kadınlar için spor egzersizi"];
+const tuerkisch = ["Gönüllülerle birlikte yapılan boş zaman etkinlikleri",
+    "Zaman", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", 
+    "14:00", "Almanca kursu", "14:30", "Yaratıcı etkinlikler", 
+    "Masa oyunları", "Erkekler için spor egzersizi", "15:30",
+    "Kadınlar için spor egzersizi"];
 
-const russisch = ["Досуг с волонтерами",  
+const russisch = ["Досуг с волонтерами",
     "Время", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", 
-    "11:00", "Творческие занятия", "14:00",
-    "Настольные игры", "Курс немецкого языка", "14:30",
-    "Спортивная тренировка для мужчин", "Спортивная тренировка для мужчин", "15:30",
-    "Спортивная тренировка для женщин", "Спортивная тренировка для женщин"];
+    "14:00", "Курс немецкого языка", "14:30", "Творческие занятия", 
+    "Настольные игры", "Спортивные упражнения для мужчин", "15:30",
+    "Спортивные упражнения для женщин"];
 
-const arabisch = ["أنشطة ترفيهية مع متطوعين", 
-    "الوقت", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة",
-    "11:00", "أنشطة إبداعية", "14:00",
-    "ألعاب الطاولة", "دورة اللغة الألمانية", "14:30",
-    "تدريب رياضي للرجال", "تدريب رياضي للرجال", "15:30",
-    "تدريب رياضي للنساء", "تدريب رياضي للنساء"];
+const arabisch = ["أوقات الفراغ مع المتطوعين",
+    "الوقت", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", 
+    "الساعة 14:00", "دورة اللغة الألمانية", "الساعة 14:30", "أنشطة إبداعية", 
+    "ألعاب لوحية", "رياضة للرجال", "الساعة 15:30",
+    "رياضة للنساء"]
 
-const farsi = ["فعالیت‌های اوقات فراغت با داوطلبان", 
+const farsi = ["فعالیت‌های اوقات فراغت با داوطلبان",
     "زمان", "دوشنبه", "سه‌شنبه", "چهارشنبه", "پنجشنبه", "جمعه",
-    "۱۱:۰۰ صبح", "فعالیت‌های خلاقانه", "۲:۰۰ بعد از ظهر",
-    "بازی‌های تخته‌ای", "دوره زبان آلمانی", "۲:۳۰ بعد از ظهر",
-    "ورزش مردان", "ورزش مردان", "۳:۳۰ بعد از ظهر",
-    "ورزش زنان", "ورزش زنان"];
+    "ساعت ۲ بعد از ظهر", "دوره زبان آلمانی", "ساعت ۲:۳۰ بعد از ظهر", "فعالیت خلاقانه",
+    "بازی‌های تخته‌ای", "ژیمناستیک برای آقایان", "ساعت ۳:۳۰ بعد از ظهر",
+    "ژیمناستیک برای خانم‌ها"];
 
 // Zuordnung Sprachen
 
@@ -71,7 +64,7 @@ const titles = ["Aktivitäten", "Activities", "activités", "faaliyetler", "де
 // Zuordnung Begriffe 
 
 function setLanguage() {
-    for (let index = 0; index < 18; index++) {
+    for (let index = 0; index < 15; index++) {
         id = String(index);
         document.getElementById(id).innerHTML = liste[index];
     }
@@ -93,7 +86,7 @@ function browserLanguageSet() {
                 document.title = titles[index];
             }
         }
-        
+
         // Fallback auf Englisch
         if (liste.length === 0) {
             liste = englisch;
@@ -132,6 +125,3 @@ document.getElementById('languageSelect').addEventListener('change', function() 
     selectLanguageSet();
     browserLanguageSet();
 });
-
-
-
